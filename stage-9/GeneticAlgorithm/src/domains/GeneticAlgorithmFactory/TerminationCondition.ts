f1f97d3426df657eefb087d00ff1cfe5c9716f43
Ken <futureappworker@gmail.com>
2026-08-14 10:47:31 +0800

    feat: add stage-9, TerminationCondition

---
+++ stage-9/GeneticAlgorithm/src/domains/GeneticAlgorithmFactory/TerminationCondition.ts
@@ -0,0 +1,11 @@
+import { Population } from "../Population";
+
+export class TerminationCondition {
+  shouldTerminate(_population : Population) : boolean {
+    // 判斷基因演算法是否已達到停止條件，決定是否結束演化
+
+    // 預設 回 false
+    // 有特別情境時，再另外繼承
+    return false
+  }
+}
